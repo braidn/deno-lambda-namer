072bf41be8580f56e5ae6c69d74525eb884d002e
Braden Douglass <braden.douglass@gmail.com>
2020-10-11 13:32:31 -0400

    Expose A Few More Deno/TS Features.
    Make it more painfully clear that this lambda is executing
    TypeScript code without the need of a compiler step.
    Either the TypeScript compiler itself or through something like webpack.
    
    Utilize typed function calls, enums, and interfaces along
    with some good ole fashion destructuring to build the return object.

--- src/hello/kamal.ts
+++ src/hello/kamal.ts
@@ -3,16 +3,46 @@ import {
   APIGatewayProxyResult,
   Context,
 } from "https://deno.land/x/lambda/mod.ts";
-handler;
+
 export async function handler(
   event: APIGatewayProxyEvent,
   context: Context,
 ): Promise<APIGatewayProxyResult> {
+  const human = buildHumi("Kamal");
+
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/json" },
     body: JSON.stringify({
-      "hello": "kamal-from-deno",
+      ...human,
+      ...{ "runningOn": "Deno", "version": Deno.version.deno },
     }),
   };
+}
+
+enum Location {
+  BROOK = "Brooklyn",
+  MANHAT = "Manhattan",
+  GREENS = "Greensboro",
+}
+
+interface Humi {
+  name: string;
+  locale: Location;
+}
+
+function buildHumi(name: string): Humi {
+  const locale = randoLocale(Location);
+
+  return {
+    name,
+    locale,
+  };
+}
+
+function randoLocale<T>(params: T): T[keyof T] {
+  const vals = Object.values(params);
+  const idx = Math.floor(Math.random() * vals.length);
+
+  return vals[idx];
 }
