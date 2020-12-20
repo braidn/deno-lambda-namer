6124133012a713cdbb5eadac4aa6e91bafccdd4b
Braden Douglass <braden.douglass@gmail.com>
2020-12-19 23:11:20 -0500

    Better Imports.
    Reach for a better naming scheme around these items. Import just what is
    required and allow for the naming to be a bit more explicit throughout
    this IAC.

--- lib/deno-lambda-stack.ts
+++ lib/deno-lambda-stack.ts
@@ -1,38 +1,37 @@
 import * as cdk from '@aws-cdk/core';
-import * as lambda from '@aws-cdk/aws-lambda'
-import * as gateway from '@aws-cdk/aws-apigatewayv2'
+import { Function, Runtime, Code, LayerVersion } from '@aws-cdk/aws-lambda'
+import { HttpApi, LambdaProxyIntegration, HttpMethod } from '@aws-cdk/aws-apigatewayv2'
 import { CfnOutput } from '@aws-cdk/core';
 
 export class DenoLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const deno = new lambda.LayerVersion(this, 'DenoLayer', {
-      code: lambda.Code.fromAsset('src/layer'),
-      compatibleRuntimes: [lambda.Runtime.PROVIDED],
+    const deno = new LayerVersion(this, 'DenoLayer', {
+      code: Code.fromAsset('src/layer'),
+      compatibleRuntimes: [Runtime.PROVIDED],
       license: 'Apache-2.0',
       description: 'Layer for running executing code on the Deno runtime'
     })
 
-    const humi = new lambda.Function(this, 'DenoHumiLocator', {
-      runtime: lambda.Runtime.PROVIDED,
-      code: lambda.Code.fromAsset('src/hello'),
-      handler: 'kamal.handler',
-      layers: [deno]
+    const api = new HttpApi(this, 'HumiApi')
+
+    const humiIntegration = new LambdaProxyIntegration({
+      handler: new Function(this, 'DenoHumiLocator', {
+        runtime: Runtime.PROVIDED,
+        code: Code.fromAsset('src/hello'),
+        handler: 'kamal.handler',
+        layers: [deno]
+      })
     })
 
-    const humiIntegration = new gateway.LambdaProxyIntegration({
-      handler: humi
-    })
-
-    const api = new gateway.HttpApi(this, 'HumiApi')
 
     api.addRoutes({
       path: '/kamal',
-      methods: [gateway.HttpMethod.GET],
+      methods: [HttpMethod.GET],
       integration: humiIntegration
     });
 
-    new CfnOutput(this, 'endpointUrl', { value: api.url }).toString()
+    new CfnOutput(this, 'endpointUrl', { value: api.url || '' }).toString()
   }
 }
